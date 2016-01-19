0a8861ab07465ee42de62c608d8a2a6b1c669b20
nibbles92 <droc6148@740s-pl-117.local>
2016-01-19 12:42:12 -0700

    added the controller

---
+++ src/Controller/AfternoonController.h
@@ -0,0 +1,24 @@
+/*
+ * AfternoonController.h
+ *
+ *  Created on: Jan 19, 2016
+ *      Author: droc6148
+ */
+
+#ifndef CONTROLLER_AFTERNOONCONTROLLER_H_
+#define CONTROLLER_AFTERNOONCONTROLLER_H_
+
+class AfternoonController
+{
+private:
+	int classCount;
+public:
+	AfternoonController();
+	void setClassCount(int classCount);
+	int getClassCount();
+	void start();
+};
+
+
+
+#endif /* CONTROLLER_AFTERNOONCONTROLLER_H_ */
